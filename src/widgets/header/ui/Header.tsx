c041120a021e4459f4b64a9ca43f4f429d990a82
Dan Scherbakov <shhworkmail@yandex.ru>
2024-08-30 14:25:00 +0300

    Tabs - buggy vers.: router collision

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,5 +1,8 @@
 import classes from './header.module.scss';
 import common from '@/shared/styles/shared.module.scss';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
 
@@ -10,7 +13,11 @@ export const Header = () => {
                     Logo!
                 </div>
                 <nav className={classes['header-navbar']}>
-                    Nav!
+                    <Tabs>
+                        <Tab label="Portfolio" value="/" to="/" component={Link} />
+                        <Tab label="News" value="/news" to="/news" component={Link} />
+                        <Tab label="Trash" value="/asset" to="/asset" component={Link} />
+                    </Tabs>
                 </nav>
                 <div className={classes['header-login']}>
                     Login!
