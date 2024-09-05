0a9f447dd593d1faab4497a8e2062f4adc1d2f24
Dan Scherbakov <shhworkmail@yandex.ru>
2024-09-05 08:24:03 +0300

    add table header

--- src/widgets/AssetsWidget/ui/AssetsWidget.tsx
+++ src/widgets/AssetsWidget/ui/AssetsWidget.tsx
@@ -4,7 +4,13 @@ import { AssetTable } from '@/enteties/ui';
 export const AssetsWidget = ({ header, assets }) => {
     return (
         <div>
-            <h3>{header}</h3>
+            <div className={classes['header-container']}>
+                <h3 className={classes['header']}>{header}</h3>
+                <div className={classes['header-add']}>
+                    <span className={classes['header-add-icon']}>+</span>
+                </div>
+            </div>
+            
             <div className={classes['assets-tables']}>
                 <AssetTable />
                 <AssetTable />
