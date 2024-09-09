87d090f7ef79c6c0761d97da15e55db21650d2bc
Denis Sherbakov <dsherbakov@rbc.ru>
2024-09-09 11:44:14 +0300

    Footer improvements

--- src/widgets/footer/ui/Footer.tsx
+++ src/widgets/footer/ui/Footer.tsx
@@ -1,12 +1,25 @@
+import Divider from '@mui/material/Divider/Divider';
 import classes from './footer.module.scss';
 import common from '@/shared/styles/shared.module.scss';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 export const Footer = () => {
 
     return (
         <footer className={classes['footer']}>
-            <div className={`${common['container']}`}>
-                Hello Footer!
+            <Divider />
+            <div className={`${common['container']}  ${classes['content-container']}`}>
+                <div>Проект БИТОЧЕК</div>
+                <div>2024 - настоящее время</div>
+                <div>
+                    <a href="https:/google.com/"  target='_blank'>
+                        <TelegramIcon />
+                    </a>
+                    <a href="https:/google.com/" target='_blank'>
+                        <GitHubIcon />
+                    </a>
+                </div>
             </div>
         </footer>
     )
