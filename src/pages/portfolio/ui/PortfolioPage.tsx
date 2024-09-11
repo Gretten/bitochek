016c65f3213a0352d2032e6a243f58c607d5dbba
Denis Sherbakov <dsherbakov@rbc.ru>
2024-09-11 12:15:19 +0300

    design changes

--- src/pages/portfolio/ui/PortfolioPage.tsx
+++ src/pages/portfolio/ui/PortfolioPage.tsx
@@ -1,9 +1,7 @@
 import { AssetsWidget } from "@/widgets/AssetsWidget";
-import { PerformanceWidget } from "@/widgets/PerformanceWidget";
 import { PriceWidget } from "@/widgets/PriceWidget";
 import classes from './classes.module.scss';
 import SelectVariants from "@/features/AddAssetType/ui/SelectVariants";
-import Button from "@mui/material/Button/Button";
 import { AssetsContext } from '@/features/AddAssetType'
 import { useContext } from "react";
 import { WidgetWrapper } from '@/shared/ui';
@@ -43,32 +41,32 @@ export const PortfolioPage = () => {
     ]
 
     return (
-            <div className={classes['portfolio']}>
-                <div className={classes["left-column"]}>
-                    <div className={classes["widget"]}>
-                        <WidgetWrapper header="ЦЕНА">
-                            <PriceWidget prices={prices} />
-                        </WidgetWrapper>
-                    </div>
-                    <div className={classes["widget"]}>
-                        <WidgetWrapper header="ПРОИЗВОДИТЕЛЬНОСТЬ" >
-                            <Performance />
-                        </WidgetWrapper>
-                    </div>
-                    <div className={classes['widget']}>
-                        <WidgetWrapper header="ДОБАВИТЬ АКТИВ" >
-                            <SelectVariants setVariant={addTable} />
-                        </WidgetWrapper>
-                    </div>
+        <div className={classes['portfolio']}>
+            <div className={classes["left-column"]}>
+                <div className={classes["widget"]}>
+                    <WidgetWrapper header="Цена">
+                        <PriceWidget prices={prices} />
+                    </WidgetWrapper>
                 </div>
-                <div className={classes["right-column"]}>
-                    <div className={classes["assets-widget"]}>
-                        <AssetsWidget 
-                            header="АКТИВЫ"
-                            // assets={assets}
-                        />
-                    </div>
+                <div className={classes["widget"]}>
+                    <WidgetWrapper header="Производительность" >
+                        <Performance />
+                    </WidgetWrapper>
+                </div>
+                <div className={classes['widget']}>
+                    <WidgetWrapper header="Добавить актив" >
+                        <SelectVariants setVariant={addTable} />
+                    </WidgetWrapper>
                 </div>
             </div>
+            <div className={classes["right-column"]}>
+                <div className={classes["assets-widget"]}>
+                    <AssetsWidget 
+                        header="Активы"
+                        // assets={assets}
+                    />
+                </div>
+            </div>
+        </div>
     )
 }
