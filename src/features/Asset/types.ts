97f0979ae958dccd99d8c9eba896430d417fdeb6
Denis Sherbakov <dsherbakov@rbc.ru>
2024-09-21 20:00:35 +0300

    redundant code removed

--- src/features/Asset/types.ts
+++ src/features/Asset/types.ts
@@ -1,16 +1,3 @@
-// interface Row {
-//     [key: string]: string;
-// }
-  
-// export interface Tables {
-//     rows: Row[];
-//     total: number;
-// }
-  
-// export interface State {
-//     tables: Tables | {};
-// }
-
 export type Asset = {
     price: number;
     count: number;
